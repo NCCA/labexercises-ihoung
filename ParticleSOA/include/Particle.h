d572db6c224559a6bd9b2292bb1ef855e3373b93
Huang Yiyang <s5303439@w11915.student.bournemouth.ac.uk>
2022-01-11 15:03:59 +0000

    add ParticleSOA

---
+++ ParticleSOA/include/Particle.h
@@ -0,0 +1,25 @@
+#ifndef PARTICLE_H_
+#define PARTICLE_H_
+
+#include <iostream>
+// #include <ngl/Vec3.h>
+
+// struct Particle
+// {
+//     Particle()=default;
+//     Particle(const Particle&)=default;
+//     Particle(const ngl::Vec3& _pos, const ngl::Vec3& _dir, const ngl::Vec3& _color, int _life, float _size):
+//             pos{_pos},
+//             dir{_dir},
+//             color{_color},
+//             life{_life},
+//             size{_size}{}
+//     ngl::Vec3 pos;
+//     ngl::Vec3 dir;
+//     ngl::Vec3 color;
+//     float size=1.0f;
+//     int life=0;
+//     int maxLife=10;
+// };
+
+#endif
